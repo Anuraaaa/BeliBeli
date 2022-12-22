b144ad24ed4e0ccc85f25a6772a774d8f5d7fe54
Anuraaaa <anwarfauzan75@gmail.com>
2022-12-23 00:35:35 +0700

    passing csrf token and validate data from RegisterController

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -9,19 +9,25 @@ use Inertia\Inertia;
 
 class RegisterController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return Inertia::render('Register');
+        $token = $request->session()->token();
+        $token = csrf_token();
+
+        return Inertia::render('Register', ['title' => 'Register', 'token' => $token]);
     }
 
     public function store(Request $request)
     {
         //passing data props errors tidak butuh props custom session karena ada validate unique
         $validatedData = $request->validate([
-            'name' => 'required|max:255',
+            'fullname' => 'required|max:255',
             'username' => 'required|min:3|max:255|unique:users',
             'email' => 'required|email:dns|unique:users',
-            'password' => 'required|min:5|max:255'
+            'gender' => 'required|in:laki-laki,perempuan',
+            'password' => 'required_with:repassword|same:repassword|min:5|max:255',
+            'repassword' => 'required|min:5|max:255',
+            'checkbox' => 'required'
         ]);
 
         $validatedData['password'] = Hash::make($validatedData['password']);
