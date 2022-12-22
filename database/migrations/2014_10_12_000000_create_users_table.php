002db54e4ba0de30d6d21bc98b2cd1847903d443
Anuraaaa <anwarfauzan75@gmail.com>
2022-12-23 00:39:06 +0700

    adjust nullable field in database table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,13 +15,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id_user');
-            $table->longText('fullname', 24);
+            $table->string('fullname', 124);
             $table->string('username', 24);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('type_user', 12);
-            $table->string('alamat', 124);
+            $table->string('type_user', 12)->nullable();
+            $table->string('alamat', 124)->nullable();
             $table->enum('jenis_kelamin', ['Laki-Laki', 'Perempuan']);
             $table->rememberToken();
             $table->timestamps();
