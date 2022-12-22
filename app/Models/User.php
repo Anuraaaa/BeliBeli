8278a13b0eae9b7b15db9a84e35c91e397849b52
Anuraaaa <anwarfauzan75@gmail.com>
2022-12-23 00:38:36 +0700

    add fillable item in user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,7 +18,9 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'fullname',
+        'username',
+        'jenis_kelamin',
         'email',
         'password',
     ];
