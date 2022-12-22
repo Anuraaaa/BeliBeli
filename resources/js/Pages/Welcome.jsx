3cdef216cffe0bd608cd63ca75402f0774ba69f4
Anuraaaa <anwarfauzan75@gmail.com>
2022-12-23 00:39:30 +0700

    use title with props

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -3,11 +3,11 @@ import Header from '@/Components/Header'
 import Navbar from '@/Components/Navbar'
 import React from 'react'
 
-export default function Welcome() {
+export default function Welcome(props) {
 
     return (
         <>
-        <Header/>
+        <Header title={props.title}/>
         <Navbar/>
         <div className='bg-blue-300'>
             <div className='container mx-auto px-2 sm:px-4 md:px-16 lg:px-32 py-8'>
