80d6ebf37de051989442f1ac6106c81d3a4aabd6
Anuraaaa <anwarfauzan75@gmail.com>
2022-12-28 03:07:38 +0700

    use component AllProduct, passing data from controller to prodyct component (parameter products, user)

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -1,17 +1,23 @@
 import Carousel from '@/Components/Carousel'
 import Header from '@/Components/Header'
 import Navbar from '@/Components/Navbar'
+import AllProduct from '@/Components/Product/AllProduct'
 import React from 'react'
 
 export default function Welcome(props) {
-
+    console.log('welcome props:', props);
     return (
         <>
         <Header title={props.title}/>
-        <Navbar/>
+        <Navbar user={props.isUser}/>
         <div className='bg-blue-300'>
             <div className='container mx-auto px-2 sm:px-4 md:px-16 lg:px-32 py-8'>
                 <Carousel/>
+            </div>
+        </div>
+        <div className='bg-gray-300 py-8'>
+            <div className='flex justify-center flex-col lg:flex-row lg:flex-wrap lg:items-stretch items-center gap-4 p-4'>
+                <AllProduct products={props.barangs.data} user={props.isUser}/>
             </div>
         </div>
         </>
