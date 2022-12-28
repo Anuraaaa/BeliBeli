2dba96bd97c0e7b27bd50b24a58a0dd34547b2e0
Anuraaaa <anwarfauzan75@gmail.com>
2022-12-28 13:49:21 +0700

    styling product view

--- resources/js/Components/Product/AllProduct.jsx
+++ resources/js/Components/Product/AllProduct.jsx
@@ -8,8 +8,13 @@ const isProduct = (products) => {
             <div key={i} className="card card-compact w-96 bg-base-100 shadow-xl p-4">
             <figure><img src="https://placeimg.com/400/225/arch" alt="" /></figure>
             <div className="card-body">
-                <h1 className="card-title">{data.nama_barang}</h1>
-                <h1 className="card-title">RP {data.harga}</h1>
+                <h1 className="card-title">
+                    {data.nama_barang}
+                    <br />
+                    RP {data.harga}
+                </h1>
+                <hr />
+                <p><strong>Keterangan:</strong></p>
                 <p>{data.keterangan}</p>
                 <div className="card-actions justify-end">
                 <button className="btn btn-md btn-ghost bg-blue-400 hover:bg-blue-600 focus:bg-blue-400">
