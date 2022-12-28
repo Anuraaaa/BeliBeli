6dc93729ff42862df6497d96a41791faffc5d573
Anuraaaa <anwarfauzan75@gmail.com>
2022-12-28 13:03:06 +0700

    remove validation isUser in AllProduct because has middleware

--- resources/js/Components/Product/AllProduct.jsx
+++ resources/js/Components/Product/AllProduct.jsx
@@ -1,39 +1,26 @@
-const isProduct = (products, user) => {
-    console.log('isProduct props', products, user)
+import cart from '/public/img/cart2.png'
+
+const isProduct = (products) => {
+    console.log('isProduct props', products)
     
-    if (user == "login") 
-    {
-        return products.map((data, i) => {
-            return (
-                <div key={i} className="card card-compact w-96 bg-base-100 shadow-xl p-4">
-                <figure><img src="https://placeimg.com/400/225/arch" alt="" /></figure>
-                <div className="card-body">
-                    <h1 className="card-title">{data.nama_barang}</h1>
-                    <h2>RP {data.harga}</h2>
-                    <p>{data.keterangan}</p>
-                    <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Buy Now</button>
-                    </div>
+    return products.map((data, i) => {
+        return (
+            <div key={i} className="card card-compact w-96 bg-base-100 shadow-xl p-4">
+            <figure><img src="https://placeimg.com/400/225/arch" alt="" /></figure>
+            <div className="card-body">
+                <h1 className="card-title">{data.nama_barang}</h1>
+                <h1 className="card-title">RP {data.harga}</h1>
+                <p>{data.keterangan}</p>
+                <div className="card-actions justify-end">
+                <button className="btn btn-md btn-ghost bg-blue-400 hover:bg-blue-600 focus:bg-blue-400">
+                    <img src={cart} alt="" className='h-6 mr-2'/>
+                    Pesan
+                    </button>
                 </div>
-                </div>        
-            )
-        })
-    }
-    else
-    {
-        return products.map((data, i) => {
-            return (
-                <div key={i} className="card card-compact w-96 bg-base-100 shadow-xl p-4">
-                <figure><img src="https://placeimg.com/400/225/arch" alt="" /></figure>
-                <div className="card-body">
-                    <h1 className="card-title">{data.nama_barang}</h1>
-                    <h2>RP {data.harga}</h2>
-                    <p>{data.keterangan}</p>
-                </div>
-                </div>
-            )
-        })
-    }
+            </div>
+            </div>        
+        )
+    })
 }
 
 
@@ -46,8 +33,8 @@ const noProduct = () => {
 }
 
 
-const AllProduct = ({products, user}) => {
-    return !products? noProduct() : isProduct(products, user)
+const AllProduct = ({products}) => {
+    return !products? noProduct() : isProduct(products)
 }
 
 export default AllProduct
